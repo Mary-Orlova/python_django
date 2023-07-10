c3b037e9e0f07dff88f3daf9070c6d50761e3c9b
Maria Orlova <vampusha07@gmail.com>
2023-07-10 21:55:37 +0300

    add 10 module

--- M_10_Testing/mysite/shopapp/tests.py
+++ M_10_Testing/mysite/shopapp/tests.py
@@ -1,3 +1,73 @@
+from django.contrib.auth.models import User, Permission
 from django.test import TestCase
+from django.urls import reverse
+from shopapp.models import Order
 
-# Create your tests here.
+
+class OrderDetailView(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='user_test', password='qwerty123', is_staff=True)
+
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls.user.delete()
+
+
+    def setUp(self):
+        self.client.force_login(self.user)
+        permission = Permission.objects.get(codename='view_order')
+        self.user.user_permissions.add(permission)
+        self.order = Order.objects.create(
+            delivery_address="ul Pupkina, d 8",
+            promocode="SALE123",
+            user=self.user,
+        )
+
+
+    def tearDown(self):
+       self.order.delete()
+
+
+class OrdersExportTestCase(TestCase):
+    fixtures = [
+        'users-fixtures.json',
+        'products-fixtures.json',
+        'orders-fixtures.json',
+    ]
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='user_test', password='qwerty123', is_staff=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls.user.delete()
+
+    def setUp(self):
+        self.client.force_login(self.user)
+
+
+    def test_get_orders_view(self):
+        response = self.client.get(reverse('shopapp:orders_export'))
+        self.assertEqual(response.status_code, 200)
+        orders = Order.objects.order_by('pk').all()
+        expected_data = [
+            {
+                'pk': order.pk,
+                'delivery_address': order.delivery_address,
+                'promocode': order.promocode,
+                'user': order.user,
+                'product': order.product,
+            }
+            for order in orders
+        ]
+
+        orders_data = response.json()
+        self.assertEqual(orders_data['orders'], expected_data)
+
--- M_10_Testing/mysite/shopapp/urls.py
+++ M_10_Testing/mysite/shopapp/urls.py
@@ -4,11 +4,15 @@ from .views import (
     ShopIndexView,
     ProductDetailsView,
     ProductsListView,
-    OrdersListView,
+    OrderListView,
+    OrderCreateView,
     OrderDetailView,
+    OrderUpdateView,
+    OrderDeleteView,
     ProductCreateView,
     ProductUpdateView,
     ProductDeleteView,
+    OrdersDataExportView,
 )
 
 app_name = "shopapp"
@@ -20,6 +24,11 @@ urlpatterns = [
     path("products/<int:pk>/", ProductDetailsView.as_view(), name="product_details"),
     path("products/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
     path("products/<int:pk>/archive/", ProductDeleteView.as_view(), name="product_delete"),
-    path("orders/", OrdersListView.as_view(), name="orders_list"),
-    path("orders/<int:pk>/", OrderDetailView.as_view(), name="order_details"),
+
+    path('orders/', OrderListView.as_view(), name='orders_list'),
+    path("orders/<int:pk>/", OrdersDataExportView.as_view(), name="orders_export"),
+    path('orders/create/', OrderCreateView.as_view(), name='order_create'),
+    path('orders/<int:pk>/',  OrderDetailView.as_view(), name='order_details'),
+    path('orders/<int:pk>/update/', OrderUpdateView.as_view(), name='order_update'),
+    path('orders/<int:pk>/archive/', OrderDeleteView.as_view(), name='order_delete'),
 ]
