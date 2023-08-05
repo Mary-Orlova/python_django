95a3c55ddc8f6662d6f0c72923f17fda5e965d5d
Мария Орлова <vampusha07@gmail.com>
2023-08-05 13:07:47 +0300

    update 13 module(settings)

--- M_13_DRF/mysite/mysite/settings.py
+++ M_13_DRF/mysite/mysite/settings.py
@@ -139,5 +139,5 @@ LOGIN_URL = reverse_lazy("myauth:login")
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_FILTER_BACKENDS': 'django_filters.rest_framework.DjangoFilterBackend',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
